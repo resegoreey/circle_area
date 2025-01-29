9e18878c58a1cf91417e73e902b947541adc915a
RESEGO MOTLHASI <remotlhjhb024@student.wethinkcode.co.za>
2025-01-29 11:56:31 +0200

    Added comments and stored the pi value in a variable

--- area.py
+++ area.py
@@ -1,4 +1,10 @@
+#User provides the radius of the circle
 radius = int(input("Provide the radius of the circle: "))
-area = 3.14 * radius**2
+
+#stores the value of pi
+PI = 3.14
+
+#calculates the area using the formula Area = π * radius^2
+area = PI * (radius ** 2)
 print(f"""The radius of your circle is {radius}
 The calculated area of your circle is {area}""")
