a77b7160cd061729cc8f5cdcf6933e4b8555d443
RESEGO MOTLHASI <remotlhjhb024@student.wethinkcode.co.za>
2025-02-21 20:55:28 +0200

    Used the math module to calculate the pi

--- area.py
+++ area.py
@@ -1,10 +1,11 @@
-#User provides the radius of the circle
-radius = int(input("Provide the radius of the circle: "))
+import math
 
-#stores the value of pi
-PI = 3.14
+def circle_area():
+    #User provides the radius of the circle
+    radius = int(input("Provide the radius of the circle: "))
 
-#calculates the area using the formula Area = π * radius^2
-area = PI * (radius ** 2)
-print(f"""The radius of your circle is {radius}
-The calculated area of your circle is {area}""")
+
+    #calculates the area using the formula Area = π * radius^2
+    area = math.pi * (radius ** 2)
+    print(f"""The radius of your circle is {radius}
+    The calculated area of your circle is {area}""")
